dbf680d257511cdf83a13995dfe221aeb9a7674f
revol-xut <revol-xut@protonmail.com>
2022-08-18 22:08:07 +0200

    hardening and increasing verbosity

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,12 @@ use clap::Parser;
 use std::env;
 use std::fs;
 use std::fs::File;
+use std::path::Path;
 use std::io::Write;
 use std::ops::Deref;
 use std::sync::{Arc, Mutex};
 use serde::{Deserialize, Serialize};
+use chrono::NaiveDateTime;
 use dump_dvb::telegrams::r09::R09SaveTelegram;
 use dump_dvb::measurements::{MeasurementInterval, FinishedMeasurementInterval};
 
@@ -24,6 +26,7 @@ struct LineInfo {
 #[derive(Deserialize, Serialize)]
 struct Response {
     success: bool,
+    time: NaiveDateTime
 }
 
 async fn start(current_run: web::Data<Arc<Mutex<MeasurementInterval>>>) -> impl Responder {
@@ -31,7 +34,7 @@ async fn start(current_run: web::Data<Arc<Mutex<MeasurementInterval>>>) -> impl 
     unlocked.start = Some(Utc::now().naive_utc());
 
     println!("entering vehicle at : {:?}", &unlocked.start);
-    web::Json(Response { success: true })
+    web::Json(Response { success: true, time: unlocked.start.unwrap() })
 }
 
 async fn stop(current_run: web::Data<Arc<Mutex<MeasurementInterval>>>) -> impl Responder {
@@ -41,21 +44,46 @@ async fn stop(current_run: web::Data<Arc<Mutex<MeasurementInterval>>>) -> impl R
     let mut unlocked = current_run.lock().unwrap();
 
     if unlocked.start == None || unlocked.line == None {
-        return web::Json(Response { success: false });
+        return web::Json(Response { success: false, time: Utc::now().naive_utc()});
     }
 
     unlocked.stop = Some(Utc::now().naive_utc());
     println!("leaving vehicle at : {:?}", &unlocked.stop);
 
+    match fs::create_dir_all("/var/lib/wartrammer-40k/") {
+        Ok(_) => {
+            println!("Successfully creates directories ... ");
+        },
+        Err(e) => {
+            println!("Did not create directories because of {:?}", e);
+        }
+    };
+
+    let path_time_file = Path::new(&time_file);
+    if !path_time_file.exists() {
+        println!("time file at: {} doesn't exist trying to create it.", &time_file);
+        let _file = File::create(&time_file).expect("Cannot create file");
+    }
+
     let data = fs::read_to_string(&time_file).expect("Unable to read file");
-    let mut res: Vec<MeasurementInterval> = serde_json::from_str(&data).expect("Unable to parse");
+
+    let mut res: Vec<MeasurementInterval>;
+    match serde_json::from_str(&data) {
+        Ok(data) => {
+            res = data;
+        }
+        Err(_) => {
+            res = Vec::new();
+        }
+    }
+
     res.push(unlocked.deref().clone());
     let raw_string = serde_json::to_string(&res).unwrap();
 
-    let mut file = File::create(&time_file).unwrap();
-    writeln!(&mut file, "{}", raw_string).unwrap();
+    let mut file = File::create(&time_file).expect("unable to create output file");
+    writeln!(&mut file, "{}", raw_string).expect("unable to write to outout file");
 
-    web::Json(Response { success: true })
+    web::Json(Response { success: true, time: unlocked.stop.unwrap() })
 }
 
 async fn meta_data(
@@ -68,7 +96,8 @@ async fn meta_data(
     unlocked.run = Some(meta_data.run);
 
     println!("adding meta data : line: {:?} run: {:?}", &unlocked.line, &unlocked.run);
-    web::Json(Response { success: true })
+
+    web::Json(Response { success: true, time: Utc::now().naive_utc() })
 }
 
 async fn finish() -> impl Responder {
@@ -96,12 +125,14 @@ async fn finish() -> impl Responder {
     }
 
     formatted_data.retain(|record: &R09SaveTelegram| -> bool {
-        println!("{:?}", record);
         for intervall in &res {
             if intervall.fits(record) {
+                println!("keeping: {:?}", record);
                 return true;
             }
         }
+
+        println!("dropping: {:?}", record);
         return false;
     });
 
@@ -112,7 +143,7 @@ async fn finish() -> impl Responder {
         wtr.serialize(&entry).unwrap();
     }
 
-    web::Json(Response { success: true })
+    web::Json(Response { success: true, time: Utc::now().naive_utc() })
 }
 
 #[actix_web::main]
@@ -132,10 +163,6 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
         App::new()
             .app_data(current_run.clone())
-            /*.route("/line_info", web::post().to(meta_data))
-            .route("/start", web::get().to(start))
-            .route("/stop", web::get().to(stop))
-            .route("/finish", web::get().to(finish)) */
             .route("/api/line_info", web::post().to(meta_data))
             .route("/api/start", web::get().to(start))
             .route("/api/stop", web::get().to(stop))
